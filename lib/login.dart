11f69c0675956813a1fbd422fea61c860a18d098
Rushabh <20rush09abh2001@gmail.com>
2023-01-31 18:35:27 +0530

    Commit 2

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,4 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -6,7 +6,44 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '', _password = '';
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,4 +1,4 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 class Signup extends StatefulWidget {
   @override
@@ -6,9 +6,44 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '', _password = '';
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
